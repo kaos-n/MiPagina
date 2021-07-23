3d5aba13b5949e80f71b61f73abbd09ce0dc8174
Kairen <mikalyadilene@gmail.com>
2021-07-23 17:25:42 -0500

    cambie el port a una constante

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const path = require ('path');
 
+const port = procces.env.PORT || 3000;
+
 //settings
-app.set('port', procces.env.PORT || 3000);
+app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
